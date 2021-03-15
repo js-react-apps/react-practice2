358d05e8d407b1f320dec3d46255a9b448d7f51f
madhav.reddy <madhav-vl>
2021-03-15 22:53:48 +0530

    state added

--- src/Employee/Employee.js
+++ src/Employee/Employee.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
 
-class Employee extends Component {
+export default class Employee extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isActive: true,
+      status: "Working..",
+    };
+  }
+
+  switchContext = () => {
+    this.setState(() => ({
+      status: this.state.isActive ? "Working" : "Sleeping",
+      isActive: !this.state.isActive,
+    }));
+  };
+
   render() {
     return (
       <div>
         Hello {this.props.name}
         <p>Welcome onboard!!</p>
+        <hr />
+        <button onClick={this.switchContext}>Submit</button>
+        <p>Status - {this.state.status}</p>
       </div>
     );
   }
 }
 
-export default Employee;
+// export default Employee;
