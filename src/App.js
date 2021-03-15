a7eb64ac9fa5669baebabd4461482e650667ec32
madhav.reddy <madhav-vl>
2021-03-15 21:47:54 +0530

    submitting class based component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Employee from "./Employee/Employee";
+
 function App() {
   return (
     <div>
       <h1> Hello React</h1>
       <p>my first React app</p>
+      <hr />
+      <Employee />
     </div>
   );
 }
---
+++ src/Employee/Employee.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Employee extends Component {
+  render() {
+    return (
+      <div>
+        Hello xxxxxxxx
+        <p>Welcome onboard!!</p>
+      </div>
+    );
+  }
+}
+
+export default Employee;
